a8d21ee434b1204c6d40c01606dc0c2c3b7daa34
Roxy Saghri <roxysaghri@gmail.com>
2011-08-28 13:41:03 -0700

    first commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails sample application
+
+Finally opened up a github account! Taking it on a test drive with this sample app :)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '20d8fe91e21fa9725e425d9be064710b97b441ff63174b3730f362c5b4ba2182df06b063465b9d73bd01070b8d6b071f7382d36924a606c6a88c9e71fab1dfeb'
